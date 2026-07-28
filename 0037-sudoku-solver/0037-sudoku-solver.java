552e19247fb83af6e0ff73739b787ffb93d180be
Yuvraj Singh <chaudharyyuvraj311@gmail.com>
2026-07-28 22:36:05 +0530

    Time: 229 ms (56.44%), Space: 42.8 MB (71.9%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,72 @@
+class Solution {
+
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    private boolean solve(char[][] board) {
+
+        for (int row = 0; row < 9; row++) {
+
+            for (int col = 0; col < 9; col++) {
+
+                if (board[row][col] == '.') {
+
+                    for (char c = '1'; c <= '9'; c++) {
+
+                        if (isValid(board, row, col, c)) {
+
+                            board[row][col] = c;
+
+                            if (solve(board)) {
+                                return true;
+                            }
+
+                            // Backtrack
+                            board[row][col] = '.';
+                        }
+                    }
+
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char c) {
+
+        // Check row
+        for (int i = 0; i < 9; i++) {
+
+            if (board[row][i] == c) {
+                return false;
+            }
+        }
+
+        // Check column
+        for (int i = 0; i < 9; i++) {
+
+            if (board[i][col] == c) {
+                return false;
+            }
+        }
+
+        // Check 3x3 box
+        int startRow = row - row % 3;
+        int startCol = col - col % 3;
+
+        for (int i = startRow; i < startRow + 3; i++) {
+
+            for (int j = startCol; j < startCol + 3; j++) {
+
+                if (board[i][j] == c) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
